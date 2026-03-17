4de5154a179fad22debb2df2642e328b6ec3ebba
Wolfgang Huang <wolfgang.huang@adl.badw.de>
2026-03-17 17:00:59 +0100

    delete url path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/stopandgraph/',  // added
+  base: '/',  // no further path if running on a subdmain path
   plugins: [react()],
 })
